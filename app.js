a7e1ab280eab67ebec1061ad44834d5be2b3d31a
Aditya Kumar Jha <adityajha126@gmail.com>
2019-07-02 16:38:05 +0530

    handtracking is simple

---
+++ app.js
@@ -0,0 +1,11 @@
+navigator.getUserMedia = 
+  navigator.getUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+
+//select everything in my html
+const video = document.querySelector('#video')
+const audio = document.querySelector('#audio')
+const canvas = document.querySelector('#canvas')
+const context = document.getContext('2d')
